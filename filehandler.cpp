9372e05db0754606150eeca47f7ee13c04021833
Alex <alexbrence@protonmail.com>
2023-09-08 21:02:44 +0200

    added parsing for both file types

--- filehandler.cpp
+++ filehandler.cpp
@@ -3,42 +3,112 @@
 #include <QFileDialog>
 #include <QDebug>
 
+#include <cstdint>
 
-FileHandler::FileHandler()
+
+constexpr uint16_t CT_WIDTH  = 512;
+constexpr uint16_t CT_HEIGHT = 512;
+constexpr uint16_t COLOR_PALETTE_WIDTH  = 256;
+constexpr uint16_t COLOR_PALETTE_HEIGHT = 3;
+
+
+CFileHandler::CFileHandler()
 {}
 
 
-bool FileHandler::_parseCTScan()
+bool CFileHandler::_parseCTScan(const QString& strFileName)
 {
-  throw std::logic_error(std::string(__FUNCTION__) + " not implemented yet.");
+  // Open a file
+  QFile file(strFileName);
+  // TODO: should you clear the vector first?
+
+  if (!file.open(QIODevice::ReadOnly))
+  {
+    qDebug() << "Could not read the file in: " + QString(__FUNCTION__);
+    return false;
+  }
+
+  // Reserve space
+  m_v2CTScan.reserve(CT_WIDTH);
+  for (QVector<short>& v : m_v2CTScan)
+  {
+    v.reserve(CT_HEIGHT);
+  }
+  QByteArray aBytes;
+  aBytes.reserve(CT_HEIGHT);
+
+  // Read content into a vector
+  for (size_t i = 0; i < CT_WIDTH; i++)
+  {
+    aBytes = file.read(CT_HEIGHT);
+    assert(aBytes.size() != 0);
+
+    QVector<short> vBytes;
+    vBytes.reserve(aBytes.size() / sizeof(short));
+
+    // Convert bytes to short and push it into vector
+    const char* pData = aBytes.constData();
+
+    for (qsizetype i = 0; i < aBytes.size(); i += sizeof(short))
+    {
+      short value = *reinterpret_cast<const short*>(pData + i);
+      vBytes.append(std::move(value));
+    }
+    m_v2CTScan.append(vBytes);
+  }
+
+  return true;
 }
 
-bool FileHandler::_parseColorPalette()
+
+bool CFileHandler::_parseColorPalette(const QString& strFileName)
 {
-  throw std::logic_error(std::string(__FUNCTION__) + " not implemented yet.");
+  // Open a file
+  QFile file(strFileName);
+
+  if (!file.open(QIODevice::ReadOnly))
+  {
+    qDebug() << "Could not read the file in: " + QString(__FUNCTION__);
+    return false;
+  }
+
+  // Reserve space
+  m_vbyColorPalette.reserve(COLOR_PALETTE_WIDTH);
+  for (QByteArray& aBytes : m_vbyColorPalette)
+  {
+    aBytes.reserve(COLOR_PALETTE_HEIGHT);
+  }
+
+  // Read content into vector
+  for (size_t i = 0; i < COLOR_PALETTE_WIDTH; i++)
+  { // TODO: check if correct number of bytes were read
+    m_vbyColorPalette.push_back(file.read(COLOR_PALETTE_HEIGHT));
+  }
+
+  return true;
 }
 
 
-QString FileHandler::browseFile(const QString& filter, const FileType fileType)
+QString CFileHandler::browseFile(const QString& filter, const EFileType fileType)
 {
   m_bParsed = false;
-  m_fileName = QFileDialog::getOpenFileName(
+  QString strFileName = QFileDialog::getOpenFileName(
       nullptr,
       "Select a file",
       "",
       filter
   );
 
-  if (!m_fileName.isEmpty())
+  if (!strFileName.isEmpty())
   {
     switch (fileType)
     {
-      case FileType::CT_SCAN:
-        m_bParsed = _parseCTScan();
+      case EFileType::CT_SCAN:
+        m_bParsed = _parseCTScan(strFileName);
         break;
 
-      case FileType::COLOR_PALETTE:
-        m_bParsed = _parseColorPalette();
+      case EFileType::COLOR_PALETTE:
+        m_bParsed = _parseColorPalette(strFileName);
         break;
 
       default:
@@ -48,11 +118,16 @@ QString FileHandler::browseFile(const QString& filter, const FileType fileType)
     }
   }
 
-  return m_fileName;
+  return strFileName;
 }
 
 
-bool FileHandler::isFileValid() const
+bool CFileHandler::areFilesReady() const
 {
   return m_bParsed;
+}
+
+QImage& CFileHandler::generateImage()
+{
+  throw std::logic_error("Not implemented yet");
 }
--- filehandler.h
+++ filehandler.h
@@ -1,43 +1,51 @@
-#ifndef FILEHANDLER_H
-#define FILEHANDLER_H
+#pragma once
+
 
 #include <QWidget>
+#include <QFile>
+#include <QVector>
+#include <QByteArray>
 
 /**
  * Class that handles browsing and loading files. It also handles
  * merging image and color palette to generate a single image.
  */
-class FileHandler
+class CFileHandler
 {
 public:
-  enum class FileType {
+  enum class EFileType {
     CT_SCAN,
     COLOR_PALETTE
   };
 
-  FileHandler();
+  CFileHandler();
 
   /**
    * @brief  Opens a file dialog and returns file path of the chosen file
    * @param  filter  filter files to specific needs
    * @return QString file name with absolute path
    */
-  QString browseFile(const QString& filter, const FileType fileType);
+  QString browseFile(const QString& filter, const EFileType fileType);
 
   /**
    * @brief Returns whether a file has been successfully parsed
    * @return bool true if file was parsed successfully, false otherwise
    */
-  bool isFileValid() const;
+  bool areFilesReady() const;
+
+  QImage& generateImage();
 
 
 private:
-  QString m_fileName;
-  FileType m_fileType;
+  QString m_strFileNameCT;
+  QString m_strFileNameColor;
+  // TODO: reserve space in the constructor
+  QVector<QVector<short>> m_v2CTScan;        // 256 * 3
+  QVector<QByteArray>     m_vbyColorPalette; // 512 * 512
   bool m_bParsed = false;
 
-  bool _parseCTScan();
-  bool _parseColorPalette();
+
+  bool _parseCTScan(const QString& strFileName);
+  bool _parseColorPalette(const QString& strFileName);
 };
 
-#endif // FILEHANDLER_H
